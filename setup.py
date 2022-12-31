afd847fccefb75b837d77eac7bcca840553511cb
alexzhou4129 <alexzhou4129@gmail.com>
2022-12-31 12:02:45 -0500

    created setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='discord-cryptocurrency-bot',
+    version='1.0.0',
+    description='My Project',
+    author='alex zhou',
+    author_email='alexzhou4129@gmail.com',
+    packages=[],
+    install_requires=[],
+)
